21c7a1a0b8717b347be4b8936191dd5db9f6031f
Yahli-Alton <80275728+Yahli-Alton@users.noreply.github.com>
2023-03-17 18:28:29 +0200

    Ex15.1 - added deleteUser to home scren

--- CountriesMainPage.aspx.cs
+++ CountriesMainPage.aspx.cs
@@ -9,18 +9,21 @@ namespace WebApplication1
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        public string msg = "";
+        public string msg = ""; 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["Admin"] == "True") { 
+            if (Session["Admin"] == "True") 
+            { 
                 msg += "<a href='Users.aspx'>Users</a>";
                 msg += "<a href='DeleteUser.aspx'>Delete User</a>";
             }
-            if (Session["uName"] == "guest") {
+            if (Session["uName"] == "guest") 
+            {
                 msg += "<a href = 'Login.aspx' > Log in</a>";
                 msg += "<a href = 'register.aspx' > register</a>";
             }
-            else {
+            else 
+            {
                 msg += "<a href = 'Logout.aspx' > Log out</a>";
                 msg += "<a href = 'updateUser.aspx' > Update User</a>";
             }
